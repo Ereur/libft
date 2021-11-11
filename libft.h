dde41fbab3264bf6e726d53b51f8acbb083313ee
Anas Amoussaoui <aamoussa@e2r3p3.1337.ma>
2021-11-11 17:45:28 +0100

    libft done

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aamoussa <aamoussa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 16:13:51 by aamoussa          #+#    #+#             */
-/*   Updated: 2021/11/10 23:21:08 by aamoussa         ###   ########.fr       */
+/*   Updated: 2021/11/11 17:10:35 by aamoussa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define LIBFT_H
 # include <string.h>
 # include <stdlib.h>
+# include <unistd.h>
 
 int		ft_isdigit(int c);
 int		ft_isalpha(int c);
@@ -44,4 +45,10 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_strdup(const char *src);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+
 #endif
